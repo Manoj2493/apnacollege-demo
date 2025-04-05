300f901e5652e52ad855217401bb0f0dba56c1f4
manoj2493y <manoj2493y@gmail.com>
2025-04-05 13:45:38 +0530

    New python file add

---
+++ Swape function.py
@@ -0,0 +1,39 @@
+a = "HeLLo wOrLd"
+print(a.swapcase())
+# print(a.islower())
+# print(a.lower())
+# print(a.upper())
+
+# def upper():
+#     for i in a:
+#         if i.islower():
+#             print(f"lower {i.upper()}")
+
+# def lower():
+#     for j in a:
+#         if j.isupper():
+#             print(f"upper {j.lower()}")
+
+def split():
+    print("-"*30)
+
+# upper()
+# split()
+# lower()
+
+
+# def swap_case(a):
+#     ans=""
+#     for c in a:
+#         if c.islower():
+#             ans+=c.upper()
+#         else:
+#             ans+=c.lower()
+#     return ans
+
+# swap_case(a)
+
+
+text = "Hello World!"
+
+print(text.swapcase()) 
